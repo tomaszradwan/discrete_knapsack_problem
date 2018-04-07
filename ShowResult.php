26791f6756a9adee995296bf9fe20249d288a1bc
tomaszradwan <radwan.tomasz@gmail.com>
2018-04-07 10:48:47 +0200

    ShowResult class created

---
+++ ShowResult.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tomasz
+ * Date: 2018-04-07
+ * Time: 10:05
+ */
+
+class ShowResult
+{
+    /**
+     * @param array $array
+     * @return string
+     */
+    static public function show(array $array):string
+    {
+        $id = array();
+        $amount = 0;
+        $weight = 0;
+
+        if (is_array($array) && count($array) > 0) {
+            foreach ($array as $key => $val) {
+                $id[] = $key;
+                $amount +=$val[2];
+                $weight += $val[1];
+            }
+        } else {
+            die("There are no results for the entered data.");
+        }
+
+        return "Total value of selected items: " . $amount
+            . " | Total weight of selected items: " . $weight
+            . " | List of selected items: " . implode(", ", $id);
+    }
+}
